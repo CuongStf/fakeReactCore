1e7e07936f5d48ea90d14b74b3ef116704aebb04
CuongStf <nguyenmanhcuong.stf@gmail.com>
2019-10-17 08:01:46 +0700

    #update: test virtual DOM

--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+(function() {
+  console.log("You are now ready to use this for development");
+})();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { updateElement } from "../virtualDOM";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const _parent = document.getElementsByClassName("vdom")[0];
+const _btn = document.getElementsByClassName("btnRerender")[0];
+
+let oldVDOM = {
+  type: "div",
+  props: { class: "parent" },
+  children: [
+    { type: "div", props: {}, children: ["1"] },
+    { type: "div", props: {}, children: ["3"] },
+    {
+      type: "div",
+      props: { class: "child" },
+      children: [
+        { type: "div", props: {}, children: ["5"] },
+        { type: "div", props: {}, children: ["6"] }
+      ]
+    }
+  ]
+};
+
+let newVDOM = {
+  type: "div",
+  props: { class: "parent" },
+  children: [
+    { type: "div", props: {}, children: ["1"] },
+    { type: "div", props: {}, children: ["2"] },
+    { type: "div", props: {}, children: ["3"] }
+  ]
+};
+
+updateElement(_parent, oldVDOM);
+_btn.addEventListener("click", () => {
+  updateElement(_parent, newVDOM, oldVDOM);
+  oldVDOM = { ...newVDOM };
+  console.log("old:", oldVDOM);
+  newVDOM = {
+    ...newVDOM,
+    ...{
+      children: [
+        ...newVDOM.children,
+        {
+          type: "div",
+          props: {},
+          children: ["Nguyen Manh Cuong"]
+        }
+      ]
+    }
+  };
+  console.log("new:", newVDOM);
+});
